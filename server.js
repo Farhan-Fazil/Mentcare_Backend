3c74e102c7efd0f95d53ff6c934d01ec979ea4d2
Farhan_Faz <113784191+Farhan-Fazil@users.noreply.github.com>
2026-01-19 20:42:54 +0530

    update cors again

--- server.js
+++ server.js
@@ -20,8 +20,7 @@ app.use(express.json())
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "https://mentcare.vercel.app"
+      "http://localhost:5173"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
